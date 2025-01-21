90ea7852bb3a2d2b06ffc561817ac43c81112c86
Andre-Usui <prof.andreusui@gmail.com>
2025-01-21 17:38:48 -0300

    Adding vercel configurations

--- api/middleware/csrf.js
+++ api/middleware/csrf.js
@@ -14,7 +14,7 @@ module.exports = {
   cookieOptions: {
     sameSite: 'lax',
     secure: true,
-    //secure: false,
+    //secure: false, // for development
     maxAge: 1000 * 60 * 60 * 24
   }
 });
